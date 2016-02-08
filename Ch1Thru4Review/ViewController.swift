08fcda820f05c9e8e30bd2f9f3a28f9c84d8f50f
Katie Riley <katie.ann.riley@hotmail.com>
2016-02-08 14:12:52 -0800

    added actions and outlets for everything on the storyboard, now able to update count using update count button and segmented control

--- Ch1Thru4Review/ViewController.swift
+++ Ch1Thru4Review/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    @IBOutlet weak var myCountLabel: UILabel!
+    @IBOutlet weak var mySegmentedControlCountIncrement: UISegmentedControl!
+    @IBOutlet weak var myNameTextField: UITextField!
+    @IBOutlet weak var myNumberTextField: UITextField!
+    @IBOutlet weak var mySwitch: UISwitch!
+    @IBOutlet weak var mySlider: UISlider!
+    
+    @IBAction func doIncremetCount(sender: AnyObject) {
+        
+        switch mySegmentedControlCountIncrement.selectedSegmentIndex{
+        case 0:
+            count++
+        case 1:
+            count += 2
+        case 2:
+            count += 3
+        default:
+            return
+        }
+        
+        updateCountLabel()
+    }
+    
+    func updateCountLabel(){
+        myCountLabel.text = "Count = " + String(count)
+    }
+    
+    @IBAction func doTapGesture(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mySwitch.on = false
+        mySlider.value = 0.0
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func doSegementedChangeBackground(sender: AnyObject) {
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +57,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func doShowInAlertView(sender: AnyObject) {
+    }
+
+    @IBAction func doMySwitch(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doClearCount(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doSlider(sender: AnyObject) {
+        
+    }
 
 }
 
